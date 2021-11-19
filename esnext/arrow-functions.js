e271bd0c77939ba0beadc00eda0c67eff14d74e6
IceOnFire <antony.mistretta@gmail.com>
2021-11-19 13:00:42 +0100

    Explain let and const and prepare for arrow funcs

---
+++ esnext/arrow-functions.js
@@ -0,0 +1,17 @@
+// function declaration
+
+function sum(a, b) {
+  return a + b;
+}
+
+// function expression
+
+const sum = function (a, b) {
+  return a + b;
+};
+
+// arrow functions
+
+const sum = (a, b) => {
+  return a + b;
+};
---
+++ esnext/let-const.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let a = 2;
+
+const b = 3;
+
+// 1. block-level scoped
+// 2. no hoisting
+// 3. no global variables
