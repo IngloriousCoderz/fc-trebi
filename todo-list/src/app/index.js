c806202057e9b857c9ca84da66a487e7829df158
IceOnFire <antony.mistretta@gmail.com>
2021-12-10 09:50:51 +0100

    Complete react app creation

--- todo-list/src/app/index.js
+++ todo-list/src/app/index.js
@@ -8,24 +8,57 @@ function App({ name }) {
     { id: 3, text: "Forget everything" },
   ]);
 
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    setTasks(addTask(tasks, text));
+    setText("");
+  };
 
-    setTasks([...tasks, { id: 4, text: "New task" }]);
+  const addTask = (tasks, text) => {
+    const maxId = tasks.length ? tasks[tasks.length - 1].id : 0;
+    return [...tasks, { id: maxId + 1, text }];
+  };
+
+  const handleSpanClick = (id) => (event) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleButtonClick = (id) => (event) => {
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   return (
     <>
       <h1>{name}'s To-Do List</h1>
       <form onSubmit={handleSubmit}>
-        <input placeholder="What next?" autoFocus />
+        <input
+          placeholder="What next?"
+          autoFocus
+          value={text}
+          onChange={handleChange}
+        />
       </form>
       <ul>
         {tasks.map(({ id, text, completed }) => (
           <li key={id}>
-            <span className={completed ? classes.completed : null}>{text}</span>
+            <span
+              className={completed ? classes.completed : null}
+              onClick={handleSpanClick(id)}
+            >
+              {text}
+            </span>
             &nbsp;
-            <button>x</button>
+            <button onClick={handleButtonClick(id)}>x</button>
           </li>
         ))}
       </ul>
