70e40cb5ab8d7ffee91687e0c721400a9b5a3a66
IceOnFire <antony.mistretta@gmail.com>
2021-12-26 10:11:19 +0100

    Move db-related logic to its own module

--- todo-list/server/routes/tasks.js
+++ todo-list/server/routes/tasks.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const debug = require('debug')('server:server')
+const db = require('../db/index')
 
 const { Server } = require('ws')
-
-const tasks = [
-  { id: 1, text: 'Learn Express', completed: true },
-  { id: 2, text: 'Look for a job', completed: false },
-  { id: 3, text: 'Forget everything' },
-]
 
 let socket = null
 
@@ -16,52 +11,53 @@ const pushServer = new Server({ port: process.env.SOCKET_PORT || 3002 })
 pushServer.on('connection', (s) => {
   socket = s
   debug('Socket server sending initial data')
+  const tasks = db.find()
   socket.send(JSON.stringify(tasks))
 })
 
 router.get('/', (req, res) => {
-  socket?.send(JSON.stringify(tasks))
+  const tasks = db.find()
+  socket?.send(JSON.stringify(db.find()))
   res.send(tasks)
 })
 
 router.get('/:id', (req, res) => {
-  const index = findIndex(tasks, req)
+  const id = Number(req.params.id)
+  const tasks = db.find()
+  const task = db.findOne(id)
   socket?.send(JSON.stringify(tasks))
-  res.send(tasks[index])
+  res.send(task)
 })
 
 router.post('/', (req, res) => {
-  const maxId = tasks.length ? tasks[tasks.length - 1].id : 0
-  const task = { ...req.body, id: maxId + 1 }
-  tasks.push(task)
+  const task = db.create(req.body)
+  const tasks = db.find()
   socket?.send(JSON.stringify(tasks))
   res.send(task)
 })
 
 router.put('/:id', (req, res) => {
-  const index = findIndex(tasks, req)
-  tasks[index] = { ...req.body, id: tasks[index].id }
+  const id = Number(req.params.id)
+  const task = db.replace(id, req.body)
+  const tasks = db.find()
   socket?.send(JSON.stringify(tasks))
-  res.send(tasks[index])
+  res.send(task)
 })
 
 router.patch('/:id', (req, res) => {
-  const index = findIndex(tasks, req)
-  tasks[index] = { ...tasks[index], ...req.body }
+  const id = Number(req.params.id)
+  const task = db.update(id, req.body)
+  const tasks = db.find()
   socket?.send(JSON.stringify(tasks))
-  res.send(tasks[index])
+  res.send(task)
 })
 
 router.delete('/:id', (req, res) => {
-  const index = findIndex(tasks, req)
-  const task = tasks[index]
-  tasks.splice(index, 1)
+  const id = Number(req.params.id)
+  const task = db.delete(id)
+  const tasks = db.find()
   socket?.send(JSON.stringify(tasks))
   res.send(task)
 })
 
 module.exports = router
-
-function findIndex(tasks, req) {
-  return tasks.findIndex(({ id }) => id === Number(req.params.id))
-}
